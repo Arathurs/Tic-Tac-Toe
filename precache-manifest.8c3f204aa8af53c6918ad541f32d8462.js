c533b1234ab970d7f0526ebef3fbee53541dbfa3
Royalthius <arathurs@gmail.com>
2020-08-13 21:38:55 -0400

    Updates

---
+++ precache-manifest.8c3f204aa8af53c6918ad541f32d8462.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d04cba34b5c1ab62291ef2e8899bc66",
+    "url": "/Tic-Tac-Toe/index.html"
+  },
+  {
+    "revision": "2d210344f32edc89cacc",
+    "url": "/Tic-Tac-Toe/static/css/main.add6a509.chunk.css"
+  },
+  {
+    "revision": "3ef48e807176bc847fa8",
+    "url": "/Tic-Tac-Toe/static/js/2.85148524.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Tic-Tac-Toe/static/js/2.85148524.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d210344f32edc89cacc",
+    "url": "/Tic-Tac-Toe/static/js/main.b0513fa9.chunk.js"
+  },
+  {
+    "revision": "5965f36d7f72120b2bad",
+    "url": "/Tic-Tac-Toe/static/js/runtime-main.49965c0b.js"
+  }
+]);
